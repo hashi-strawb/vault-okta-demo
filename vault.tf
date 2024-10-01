00e9bbc90b1ab59ad7ea7c16494444f79959158b
Lucy Davinhart <lucy@lucymhdavies.com>
2024-10-01 17:25:22 +0100

    add a separate admin OIDC role, as a test

--- vault.tf
+++ vault.tf
@@ -109,3 +109,66 @@ resource "vault_identity_group_alias" "group-alias" {
   mount_accessor = vault_jwt_auth_backend.okta_oidc.accessor
   canonical_id   = vault_identity_group.group[each.key].id
 }
+
+
+
+
+resource "vault_jwt_auth_backend_role" "okta_role_admin" {
+  backend        = vault_jwt_auth_backend.okta_oidc.path
+  role_name      = "admin"
+  token_policies = ["superadmin"]
+
+  allowed_redirect_uris = [
+    "${var.vault_addr}/ui/vault/auth/${vault_jwt_auth_backend.okta_oidc.path}/oidc/callback",
+    # This is for logging in with the CLI if you want.
+    "http://localhost:${var.cli_port}/oidc/callback",
+  ]
+
+  user_claim = "email"
+  #user_claim      = "sub"
+  role_type       = "oidc"
+  bound_audiences = [var.okta_auth_audience, okta_app_oauth.vault.client_id]
+  # bound_audiences = [okta_auth_server.vault.audiences]
+  oidc_scopes = [
+    "openid",
+    "profile",
+    "email",
+  ]
+  groups_claim = "groups"
+
+  bound_claims = {
+    groups = "vault-admins"
+  }
+
+  # From https://developer.okta.com/docs/reference/api/oidc/#id-token-payload
+  claim_mappings = {
+    "name"               = "name",
+    "family_name"        = "family_name",
+    "given_name"         = "given_name",
+    "nickname"           = "nickname",
+    "preferred_username" = "preferred_username",
+    "email"              = "email",
+
+
+
+    #"ver"            = "ver",
+    "sub" = "sub",
+    "iss" = "iss",
+    "aud" = "aud",
+    #"iat"            = "iat",
+    #"exp"            = "expr",
+    "jti" = "jti",
+    #"auth_time"      = "auth_time",
+    "at_hash"  = "at_hash",
+    "profile"  = "profile",
+    "zoneinfo" = "zoneinfo",
+    "locale"   = "locale",
+    #"updated_at"     = "updated_at",
+    #"email_verified" = "email_verified",
+
+    # Can't do non-strings because of
+    # https://github.com/hashicorp/vault-plugin-auth-jwt/issues/109
+  }
+
+  verbose_oidc_logging = true
+}
